06b5002d9c57d723c076d7186ca69ebda25994b4
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 12:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 //your JS code here. If required.
-//your JS code here. If requir
-// script.js
+//your JS code here. If required.
+const p=document.getElementById("timer")
 
-function updateTimer() {
-  var timerElement = document.getElementById('timer');
+let today=new Date();
+// const arr=[]
+// arr.push(today.getDate())
+let day=today.getDate();
+let month=today.getMonth();
+let year=today.getFullYear()
+let hour=today.getHours();
+let minute=today.getMinutes()
+let second=today.getSeconds();
 
-  // Get the current date and time
-  var currentDate = new Date();
 
-  // Format the date and time
-  var date = currentDate.toLocaleDateString();
-  var time = currentDate.toLocaleTimeString();
+p.innerText=day+"/"+month+"/"+year+" ,"+hour+":"+minute+":"+second;
 
-  // Update the timer element
-  timerElement.textContent = 'Date: ' + date + ' | Time: ' + time;
-}
 
-// Update the timer every second
-setInterval(updateTimer, 1000);
-
-	
-}
